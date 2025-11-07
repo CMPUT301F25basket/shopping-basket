9ecb71cffe021d29b7df5b96cb7697646e9ff853
chauu335 <hoangchau2310@gmail.com>
2025-11-07 15:02:00 -0700

    Bug fix login_signup xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        dataBinding = true
     }
 }
 
@@ -54,4 +55,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-firestore")
+    
 }
